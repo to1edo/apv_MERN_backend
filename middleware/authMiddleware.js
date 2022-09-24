79dea1c99af57153fe7aba020ab5a73b9c918f25
carlos toledo <to1edo>
2022-09-24 06:43:35 -0400

    test middle

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,19 +1,19 @@
-import jwt from 'jsonwebtoken'
+import jwt, { decode } from 'jsonwebtoken'
 import Veterinario from '../models/Veterinario.js';
 
 async function checkAuth(req, res, next) {
 
     let token
-
+    console.log(req.headers.authorization)
     if( req.headers.authorization &&  req.headers.authorization.startsWith('Bearer') ){
         
         try {
             token = req.headers.authorization.split(' ')[1];
-
+            console.log(token)
             const decoded = jwt.verify(token, process.env.JWT_secret)
-
+            console.log(decoded)
             req.veterinario = await Veterinario.findById(decoded.id).select("-password -token -confirmado")
-            
+            console.log(req.veterinario)
             return next()
 
         } catch (error) {
